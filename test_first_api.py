c9223c6464d68c899aa5a5ae43777b0699aa35de
GlebAQA <galievgleb99@gmail.com>
2023-06-15 16:51:25 +0300

    add test

--- test_first_api.py
+++ test_first_api.py
@@ -3,10 +3,15 @@ import pytest
 
 
 class TestFirstApi:
-    def test_hello_call(self):
+    names = [
+        ("Gleb"),
+        ("Regina"),
+        ("")
+    ]
+    @pytest.mark.parametrize('name', names) # в name pytest передает данные, names переменная в которой данные хранятся
+    def test_hello_call(self, name):
         url = "https://playground.learnqa.ru/api/hello"
-        name = "Gleb"
-        data = {'name': name}
+        data = {'name':name}
 
         response = requests.get(url, params=data) # Response [200]
         assert response.status_code == 200, "Wrong status code" #200
@@ -14,6 +19,10 @@ class TestFirstApi:
         response_dict = response.json() # {'answer': 'Hello, Gleb'}
         assert "answer" in response_dict, "There is no field 'answer' in the response"
 
-        expected_response_text = f"Hello, {name}"
+        if len(name) == 0:
+            expected_response_text = "Hello, someone"
+        else:
+            expected_response_text = f"Hello, {name}"
+
         actual_response_text = response_dict["answer"] # Hello, Gleb
         assert expected_response_text == actual_response_text, f"Actual text in the response is not corrected"
